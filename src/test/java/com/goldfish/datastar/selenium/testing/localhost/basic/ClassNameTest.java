d99c624d85ed173e6cecb5f67d892f8f98d9eb56
Family <Family@192.168.1.160>
2023-12-15 23:58:02 +0100

    ClassNameTest

---
+++ src/test/java/com/goldfish/datastar/selenium/testing/localhost/basic/ClassNameTest.java
@@ -0,0 +1,31 @@
+package com.goldfish.datastar.selenium.testing.localhost.basic;
+
+import static org.testng.AssertJUnit.assertEquals;
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.annotations.Test;
+
+import com.goldfish.datastar.selenium.testing.webdriver.GoldfishDatastarAbstractChromeWebDriverTestNgTest;
+
+public class ClassNameTest extends GoldfishDatastarAbstractChromeWebDriverTestNgTest {
+
+	@Test
+	public void testTitleTextElement() {
+		driver.get("http://localhost:8080/pages/index.html");
+		WebElement title = driver.findElement(By.className("navbar-brand"));
+		assertEquals(title.getText(), "SB Admin v2.0");
+	}
+
+	@Test
+	public void testHugeTextElements() {
+		driver.get("http://localhost:8080/pages/index.html");
+		List<WebElement> hugeElements = driver.findElements(By.className("huge"));
+		for (WebElement element : hugeElements) {
+			System.out.println(element.getText());
+		}
+
+	}
+
+}
